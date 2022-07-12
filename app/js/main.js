38f71906dcdc5fc33ea09ae063d0302211df8e30
antegorov <antegorov@mail.ru>
2022-07-12 13:27:08 +0500

    added page new-items, added fonts

--- app/js/main.js
+++ app/js/main.js
@@ -7,18 +7,40 @@ $(function() {
     slidesToScroll: 4
   });
 
-
-
-
-  
-  
-
   $(".rate-star").rateYo({
     rating: 4,
     starWidth: "12px",
     readOnly: true
   });
 
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    from: 0,
+    to: 600,
+    prefix: "$",
+  });
+
+  $('.catalog__filter-button').on('click', function() {
+    $('.catalog__filter-button').removeClass('button__active');
+    $(this).addClass('button__active');
+  });
+
+  $('.icon-sort').on('click', function() {
+    $('.icon-sort').removeClass('icon-sort__active');
+    $(this).addClass('icon-sort__active');
+  });
+
+  $('.icon-th-list').on('click', function() {
+    $('.product__page-item').addClass('active__list-item')
+  });
+
+  $('.icon-th-large').on('click', function() {
+    $('.product__page-item').removeClass('active__list-item')
+  });
+
+
+
+    
 
 
 
