8e03e2ae85b41a2abb8657509140a259eca8ae21
antegorov <antegorov@mail.ru>
2022-07-16 14:10:12 +0500

    added page 404.html, about.html, contact.html, and fixed product-one.html

--- app/js/main.js
+++ app/js/main.js
@@ -16,7 +16,7 @@ $(function() {
   $(".js-range-slider").ionRangeSlider({
     type: "double",
     from: 0,
-    to: 600,
+    to: 1000,
     prefix: "$",
   });
 
@@ -47,6 +47,15 @@ $(function() {
     $('.header__box').toggleClass('active')
   });
     
+
+  $('.product-one__tabs .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+      $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+      $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+      $(this).addClass('active');
+      $('#'+id).addClass('active-tab').fadeIn();
+      return false;
+    });
 
 
 
