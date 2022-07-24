1bda680c11c2ff0292a7ed7e09d60f61e5786f3e
antegorov <antegorov@mail.ru>
2022-07-24 10:46:08 +0500

    page upload.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
 
   $('.product-slider__inner').slick({
     dots: true,
@@ -20,42 +20,44 @@ $(function() {
     prefix: "$",
   });
 
-  $('.catalog__filter-button').on('click', function() {
+  $('.catalog__filter-button').on('click', function () {
     $('.catalog__filter-button').removeClass('button__active');
     $(this).addClass('button__active');
   });
 
-  $('.icon-sort').on('click', function() {
+  $('.icon-sort').on('click', function () {
     $('.icon-sort').removeClass('icon-sort__active');
     $(this).addClass('icon-sort__active');
   });
 
-  $('.icon-th-list').on('click', function() {
+  $('.icon-th-list').on('click', function () {
     $('.product__page-item').addClass('active__list-item')
   });
 
-  $('.icon-th-large').on('click', function() {
+  $('.icon-th-large').on('click', function () {
     $('.product__page-item').removeClass('active__list-item')
   });
 
 
-  $('.menu__btn').on('click', function(){
+  $('.menu__btn').on('click', function () {
     $('.menu__list').slideToggle();
   });
 
-  $('.header__btn-menu').on('click', function(){
+  $('.header__btn-menu').on('click', function () {
     $('.header__box').toggleClass('active')
   });
-    
 
-  $('.product-one__tabs .tab, settings__tabs .tab').on('click', function(event) {
+
+  $('.product-one__tabs .tab, settings__tabs .tab').on('click', function (event) {
     var id = $(this).attr('data-id');
-      $('.product-one__tabs, settings__tabs').find('.tab-item').removeClass('active-tab').hide();
-      $('.product-one__tabs .tabs, settings__tabs .tabs').find('.tab').removeClass('active');
-      $(this).addClass('active');
-      $('#'+id).addClass('active-tab').fadeIn();
-      return false;
-    });
+    $('.product-one__tabs, settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+    $('.product-one__tabs .tabs, settings__tabs .tabs').find('.tab').removeClass('active');
+    $(this).addClass('active');
+    $('#' + id).addClass('active-tab').fadeIn();
+    return false;
+  });
+
+  $('input[type="file"], select').styler();
 
 
 
